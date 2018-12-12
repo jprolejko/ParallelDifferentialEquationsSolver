8bfb134ad683895bc4b9ec8fedf5cd6e31a8c230
jprolejko <jprolejko@gmail.com>
2018-12-12 21:54:25 +0100

    Added handle of variable in parser

--- solver/equations_parser.py
+++ solver/equations_parser.py
@@ -1,29 +1,38 @@
 class EquationsParser:
-    VARIABLE_NAME = 'x'
+    FUNCTION_NAME = 'x'
+    VARIABLE_NAME = 't'
 
-    def __init__(self, equations, initial_values):
+    def __init__(self, equations, initial_values, interval):
         self.equations = equations
         self.initial_values = initial_values
+        self.interval = interval
 
     def parse(self):
         if len(self.initial_values) != len(self.equations):
             raise AttributeError("Initial values should be the same size as equations!")
 
-        globals()[self.VARIABLE_NAME] = self.initial_values
+        if len(self.interval) != 2:
+            raise AttributeError("Wrong interval!")
+
+        if self.interval[0] > self.interval[1]:
+            raise AttributeError("Wrong interval!")
+
+        globals()[self.FUNCTION_NAME] = self.initial_values
+        globals()[self.VARIABLE_NAME] = self.interval[0]
 
         for equation_index, equation in enumerate(self.equations):
             variable_seen = False
             new_equation = ''
 
             for char in equation:
-                if char == self.VARIABLE_NAME:
+                if char == self.FUNCTION_NAME:
                     variable_seen = True
                     new_equation += char + '['
                     continue
                 elif variable_seen and not char.isnumeric():
                     new_equation += ']'
                     variable_seen = False
-                elif char.isalpha() and char != self.VARIABLE_NAME:
+                elif char.isalpha() and (char != self.FUNCTION_NAME or char != self.VARIABLE_NAME):
                     raise SyntaxError
 
                 new_equation += char
--- tests/__init__.py
+++ tests/__init__.py
@@ -9,44 +9,64 @@ class TestEquationsParser(unittest.TestCase):
         with self.assertRaises(AttributeError):
             equations = ['5', '2']
             initial_values = [0]
-            eq_parser = parser.EquationsParser(equations, initial_values)
+            interval = [0, 1]
+            eq_parser = parser.EquationsParser(equations, initial_values, interval)
             eq_parser.parse()
         with self.assertRaises(AttributeError):
             equations = ['x1 + 5', 'x1/x0']
             initial_values = [0, 2, 5]
-            eq_parser = parser.EquationsParser(equations, initial_values)
+            interval = [0, 1]
+            eq_parser = parser.EquationsParser(equations, initial_values, interval)
             eq_parser.parse()
         with self.assertRaises(AttributeError):
             equations = ['x2*x2 - x1', 'x1', '5*x2']
             initial_values = []
-            eq_parser = parser.EquationsParser(equations, initial_values)
+            interval = [0, 1]
+            eq_parser = parser.EquationsParser(equations, initial_values, interval)
+            eq_parser.parse()
+
+        with self.assertRaises(AttributeError):
+            equations = ['x2 + 5*x1*(x0-x2) + (5*12) / x1', 'x0/(x1*x2) - (23/12) * x0', 'x1/x2*x0+x1 - x2']
+            initial_values = [1, 1.2, 43.1]
+            interval = [0]
+            eq_parser = parser.EquationsParser(equations, initial_values, interval)
+            eq_parser.parse()
+        with self.assertRaises(AttributeError):
+            equations = ['x2 + 5*x1*(x0-x2) + (5*12) / x1', 'x0/(x1*x2) - (23/12) * x0', 'x1/x2*x0+x1 - x2']
+            initial_values = [1, 1.2, 43.1]
+            interval = [0, -1]
+            eq_parser = parser.EquationsParser(equations, initial_values, interval)
             eq_parser.parse()
 
     def test_out_of_bounds(self):
         with self.assertRaises(IndexError):
             equations = ['x3 + x2 - x0*12', 'x1/x2']
             initial_values = [0, 1]
-            eq_parser = parser.EquationsParser(equations, initial_values)
+            interval = [0, 1]
+            eq_parser = parser.EquationsParser(equations, initial_values, interval)
             eq_parser.parse()
 
     def test_division_by_zero(self):
         with self.assertRaises(ZeroDivisionError):
             equations = ['x1/x0', '5']
             initial_values = [0, 1]
-            eq_parser = parser.EquationsParser(equations, initial_values)
+            interval = [0, 1]
+            eq_parser = parser.EquationsParser(equations, initial_values, interval)
             eq_parser.parse()
 
     def test_wrong_syntax(self):
         with self.assertRaises(SyntaxError):
             equations = ['x1/-=x0', '5']
             initial_values = [1, 1]
-            eq_parser = parser.EquationsParser(equations, initial_values)
+            interval = [0, 1]
+            eq_parser = parser.EquationsParser(equations, initial_values, interval)
             eq_parser.parse()
 
     def test_right_syntax(self):
         equations = ['x2 + 5*x1*(x0-x2) + (5*12) / x1', 'x0/(x1*x2) - (23/12) * x0', 'x1/x2*x0+x1 - x2']
         initial_values = [1, 1.2, 43.1]
-        eq_parser = parser.EquationsParser(equations, initial_values)
+        interval = [0, 1]
+        eq_parser = parser.EquationsParser(equations, initial_values, interval)
         test_case = True
         try:
             eq_parser.parse()
