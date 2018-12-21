4d4a0f4134de6ecdf8d911345e0e5e3629c17539
jprolejko <jprolejko@gmail.com>
2018-12-21 15:18:33 +0100

    Preparation for multiprocessing

--- solver/runge_kutta_method.py
+++ solver/runge_kutta_method.py
@@ -1,5 +1,6 @@
 import solver.config as config
 from math import sin, cos, tan, sqrt, pow, log
+from multiprocessing import Process
 
 
 class RungeKuttaMethod:
@@ -11,53 +12,53 @@ class RungeKuttaMethod:
         globals()[config.FUNCTION_NAME] = initial_conditions
         globals()[config.VARIABLE_NAME] = start
 
-        end_conditions = []
+        end_conditions = [0]*len(self.equations)
         end = start
 
-        k1 = k2 = k3 = k4 = []
+        k1 = k2 = k3 = k4 = [0]*len(self.equations)
 
-        for equation in self.equations:
-            k1.append(step * eval(equation))
+        for index, equation in enumerate(self.equations):
+            k1[index] = (step * eval(equation))
 
-        for equation in self.equations:
+        for index, equation in enumerate(self.equations):
             globals()[config.VARIABLE_NAME] = start + step/2.0
 
             conditions = initial_conditions
 
-            for index, condition in enumerate(conditions):
-                condition += k1[index] / 2.0
+            for index_con, condition in enumerate(conditions):
+                condition += k1[index_con] / 2.0
 
             globals()[config.FUNCTION_NAME] = conditions
 
-            k2.append(step * eval(equation))
+            k2[index] = (step * eval(equation))
 
-        for equation in self.equations:
+        for index, equation in enumerate(self.equations):
             globals()[config.VARIABLE_NAME] = start + step / 2.0
 
             conditions = initial_conditions
 
-            for index, condition in enumerate(conditions):
-                condition += k2[index] / 2.0
+            for index_con, condition in enumerate(conditions):
+                condition += k2[index_con] / 2.0
 
             globals()[config.FUNCTION_NAME] = conditions
 
-            k3.append(step * eval(equation))
+            k3[index] = (step * eval(equation))
 
-        for equation in self.equations:
+        for index, equation in enumerate(self.equations):
             globals()[config.VARIABLE_NAME] = start + step
 
             conditions = initial_conditions
 
-            for index, condition in enumerate(conditions):
-                condition += k3[index]
+            for index_con, condition in enumerate(conditions):
+                condition += k3[index_con]
 
             globals()[config.FUNCTION_NAME] = conditions
 
-            k4.append(step * eval(equation))
+            k4[index] = (step * eval(equation))
 
         for index, equation in enumerate(self.equations):
             k = 1.0 / 6.0 * (k1[index] + 2 * k2[index] + 2 * k3[index] + k4[index])
-            end_conditions.append(initial_conditions[index] + k)
+            end_conditions[index] = (initial_conditions[index] + k)
 
         end += step
 
