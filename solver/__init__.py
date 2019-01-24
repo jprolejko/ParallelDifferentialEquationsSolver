86878fc8caef65b5cfa2897aaa76720bbf3cef81
jprolejko <jprolejko@gmail.com>
2019-01-24 01:26:38 +0100

    Added parallel saving to file

--- solver/__init__.py
+++ solver/__init__.py
@@ -1,6 +1,14 @@
 import solver.application_interface as ui
 import solver.equations_parser as parser
 import solver.runge_kutta_method as rk4
+from multiprocessing import Pool
+
+
+def append_file(time, values):
+    with open("results.txt", "a") as file:
+        file.write(str('%.4f' % time) + ", " + str(values) + '\n')
+
+    return True
 
 
 def main():
@@ -12,6 +20,7 @@ def main():
                                        app_interface.return_interval())
 
     try:
+        pool = Pool()
         eq_parser.parse()
         app_interface.print_info('Parsing passed!')
 
@@ -19,12 +28,18 @@ def main():
 
         values = eq_parser.return_initial_values()
         time = app_interface.return_interval()[0]
-        print(str('%.4f' % time) + ": " + str(values))
+        end = time
 
-        while time + app_interface.return_step() < app_interface.return_interval()[1]:
+        with open("results.txt", "w") as file:
+            file.write(str('%.4f' % time) + ", " + str(values) + '\n')
+
+        while time - app_interface.return_step() <= app_interface.return_interval()[1]:
+            result = pool.apply_async(append_file, [end, values])
             end, values = rk.compute(time, values, app_interface.return_step())
-            print(str('%.4f' % end) + ": " + str(values))
             time = end
+
+            while not result.get():
+                pass
 
     except IndexError:
         app_interface.print_error('Unknown variable used in equations!')
